f0248ee27a6a11ce1193f9dc3c931020d5625d4e
Aregbede Ayomide <ayo.aregbede@gmail.com>
2016-03-07 04:30:55 +0100

    create fileitem for and add drop files to state

--- src/App.js
+++ src/App.js
@@ -3,26 +3,46 @@ import DropZone from 'react-dropzone'
 import FileItem from './fileItem.js'
 require('./index.scss')
 export default class App extends Component {
-
+  constructor(){
+    super()
+    this.state = {files: []}
+  }
   onDrop = (files) => {
     console.log(files)
+    let newFiles = files.map(file => {
+      const file_obj = {
+        percent:0,
+        file
+      }
+      return file_obj
+    })
+
+    this.setState({files:this.state.files.concat(newFiles)})
   }
   render() {
+    const files = this.state.files;
+    console.log(files)
+    const filesInProgress = files.map(content =>  content.percent < 100 ? content : "")
     return (
       <div className="container-fluid">
          <div className="row">
                <div className="col-md-4">
                    <div className="wall">
                      <header> Files in Progress</header>
-                           <ul>
-                         <li>
-        <FileItem />
-                         </li>
+                           <ul className="list-unstyled">
+                            {
+                              filesInProgress.map(content => {
+                                return <li> <FileItem file_name={content.file.name}
+                                                      file_size={content.file.size}
+                                                      file_ext={content.file.type}
+                                                      percent={content.percent}/></li>
+                              })
+                            }
                            </ul>
                    </div>
                </div>
                <div className="col-md-4">
-                     <DropZone className="dropBox">
+                     <DropZone className="dropBox" onDrop={this.onDrop}>
                        Drag & Drop Files Here
                      </DropZone>
                </div>
---
+++ src/fileItem.js
@@ -0,0 +1,42 @@
+import React, {Component, PropTypes} from 'react'
+
+
+export default class FileItem extends Component {
+
+  render(){
+    const file_name =  this.props.file_name;
+    const file_size =  this.props.file_size;
+    const file_ext = this.props.file_ext;
+    const percent = this.props.percent;
+    const style = {
+      width:percent
+    }
+    //Math.floor(Math.random() * 100) + 10 
+    return (
+      <div className="fileitem">
+        <div>
+        </div>
+            <div className="title">
+              <span className="myLabel">File Name:</span><span>{file_name}</span>
+              <span className="myLabel">File Size:</span><span>{file_size+"kb"}</span>
+            </div>
+            <div className="title">
+              <span className="myLabel">File Ext:</span><span>{file_ext}</span>
+              <span className="myLabel">Status:</span><span className="status">upload pending</span>
+            </div>
+            <div>
+              <span className="progress" style={style}>
+              </span>
+            </div>
+      </div>
+    )
+  }
+}
+
+
+FileItem.propTypes = {
+  file_name: PropTypes.string.isRequired,
+  file_size: PropTypes.number.isRequired,
+  file_ext: PropTypes.string.isRequired,
+  percent: PropTypes.number
+}
